563baf35eaf753eee147e04e646804b4499a5515
Jaytitaya <titaya_poonpipatnun@yahoo.com>
2023-08-08 01:29:31 +0700

    [Cart] Building Cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './homepage/home/home.component';
+import { CartComponent } from './homepage/cart/cart.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
--- src/app/homepage/cart/cart.component.ts
+++ src/app/homepage/cart/cart.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit, OnDestroy {
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1,
+  }]};
+  dataSource: CartItem[] = [];
+  displayedColumns: string[] = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
 
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+  ngOnDestroy() {}
+  
+  getTotal(items: Array<CartItem>): number {
+    return items.
+      map((item)=> item.price*item.quantity)
+      .reduce((prev,current)=>prev+current,0);
+  }
 }
