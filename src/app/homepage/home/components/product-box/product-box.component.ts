96cc9d0ba10c02726093d0215b6aa12f57dda58b
Jaytitaya <titaya_poonpipatnun@yahoo.com>
2023-08-13 23:19:06 +0700

    [AddToCart]: create product model and function AddToCart in product-box

--- src/app/homepage/home/components/product-box/product-box.component.ts
+++ src/app/homepage/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -7,4 +8,18 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
+  @Output() addToCart = new EventEmitter();
+  product: Product| undefined ={
+    id: 1,
+    title: 'Snickers',
+    price: 150,
+    category: 'shoes',
+    description: 'description',
+    image: 'https://via.placeholder.com/150'
+  };
+  
+
+  onAddToCart(): void{
+    this.addToCart.emit(this.product);
+  }
 }
